f6fe3f446193522856ff7f5eebcf7dbd31c0d56a
matheushenriquenegri-eng <matheus.henrique.negri@escola.pr.gov.br>
2026-08-21 12:46:51 +0000

    AVANÇANDO NOS ALINHAMENTOS

--- lib/paginas/aula02.dart
+++ lib/paginas/aula02.dart
@@ -5,30 +5,96 @@ class Aula02 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   double _tamanhoIcones = 40;
+  // Construindo widget costumizavel (Reaproveitamento de código)
+    Widget _construirLinha(
+     MainAxisAlignment alinhamento, // Alinhamento por parâmetro
+     IconData icone1, // Icones por parâmetro
+     IconData icone2,
+     IconData icone3,
+     ) { 
+            return Container( 
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.red),
+              ),
+              // Criando uma linha que alinha seu conteúdo no início
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: alinhamento,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(icone1),
+                  ),
+                   Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(icone2),
+                  ),
+                   Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(icone3),
+                  ),
+                ],
+              ),
+            );
 
+
+    }
     return Scaffold(
       appBar: AppBar(
-      title: const Text('Aula02 - Rows e columns'),
-    ),
-    body: Conteiner(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-            Icon(Icons.home, size: _tamanhoIcones),
-            Icon(Icons.person, size: _tamanhoIcones),
-            Icon(Icons.settings, size: _tamanhoIcones),
-            ],
-          ),
-          Column(),
-          Column(),
-        ]
-      ,)
-    ),
+        title: Text("Aula 02 - Rows e Columns"),
+      ),
+      // Cria uma barra de rolagem para o conteúdo da tela
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+              child: Text("Start (Padrão)"),
+            ),
+           _construirLinha(MainAxisAlignment.start, Icons.abc, Icons.abc, Icons.abc),
+            // Definido uma coluna
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue),
+              ),
+              height: 200,
+              // Criando uma linha que alinha seu conteúdo no início
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.add_reaction),
+                  ),
+                   Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.abc_rounded),
+                  ),
+                   Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.green, width: 2),
+                    ),
+                    child: Icon(Icons.account_balance_wallet),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
